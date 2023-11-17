878b1222e53fe9911da6433a7ce1c3fab5728dde
Yaroslav Abrosimov <abrosimovyar@gmail.com>
2023-11-17 16:06:28 +0300

    Create docker container

--- Kreimer/conf/gunicorn_config.py
+++
@@ -1,4 +0,0 @@
-command = '/home/dzirtdourden/server-docker/web-tasker/Kreimer/django_env/bin/gunicorn'
-pythonpath = '/home/dzirtdourden/server-docker/web-tasker/Kreimer/webtasker'
-bind = '192.168.50.250:10000'
-workers = 1
---
+++ Kreimer/readme.md
@@ -0,0 +1,11 @@
+# Start docker image
+
+You can build image with
+```console
+docker compose build
+```
+
+Or you can start it immediately with
+```console
+docker compose up -d
+```
